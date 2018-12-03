9a8cf103f3563e2c7700812648e7cbafbc8e1c03
myhrnz1 <45091814+myhrnz1@users.noreply.github.com>
2018-12-03 13:17:57 +0100

    Update TTK_User.java
    Lade till indentering

--- TTK_User.java
+++ TTK_User.java
@@ -1,15 +1,10 @@
 public class TTK_User {
 
-String username = "";
-String password = "";
+  String username = "";
+  String password = "";
 
-
-
-TTK_User(String inputUsername, String inputPassword){
-
-username = inputUsername;
-password = inputPassword;
-
-}
-
+  TTK_User(String inputUsername, String inputPassword){
+    username = inputUsername;
+    password = inputPassword;
+  }
 }
