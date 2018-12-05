8be1a93c1730c089af6e45513a6c2a8bfebe5013
myhrnz1 <myhrnz@outlook.com>
2018-12-05 16:06:10 +0100

    add variables and methods/ALL

--- TTK_Login.java
+++ TTK_Login.java
@@ -1,3 +1,26 @@
 public class TTK_Login{
+  private String tempUsername;
+  private boolean usernameIsValid;
+
+  public TTK_Login() {
+    tempUsername = "";
+    usernameIsValid = false;
+  }
+
+  public boolean checkUsername(String username) {
+    int index = TTK_Users.indexOfUser(username);
+    if (index < TTK_Users.userList.size()) {
+      tempUsername = username;
+      usernameIsValid = true;
+    }
+    return usernameIsValid;
+  }
+
+  public boolean checkPassword(String password) {
+    if (usernameIsValid) {
+      return password.equals(TTK_Users.passwordOfUser(tempUsername));
+    }
+    return false;
+  }
   
 }
